e8076a3bb40d11e74ffc49b800ff5bd829f65061
Randy <57063714+randyisback@users.noreply.github.com>
2024-03-05 19:00:25 +0300

    First Commit
    first

---
+++ js/countries.js
@@ -0,0 +1,129 @@
+const countries = 
+{
+    "tr-TR": "Türkçe",
+    "ku-TR": "Kürtçe",
+    "en-GB": "İngilizce", 
+    "ru-RU": "Rusça",    
+    "de-DE": "Almanca",
+    "pt-PT": "Portekizce",
+    "fr-FR": "Fransızca",
+    "it-IT": "İtalyanca",
+    "es-ES": "İspanyolca",
+    "ja-JP": "Japonca",
+    "ko-KR": "Korece",
+    "la-VA": "Latince",
+    "el-GR": "Yunanca",
+    "cs-CZ": "Çekçe",
+    "hi-IN": "Hintçe",
+    "fa-IR": "Farsça",
+    "ar-SA": "Arapça",
+}
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // "et-EE": "Estonyaca",
+    // "he-IL": "İbranice",
+    // "eu-ES": "Baskça",
+    // "fi-FI": "Fince",
+    // "fn-FNG": "Fanagalo",
+    // "fo-FO": "Faroece",
+    // "gl-ES": "Galiçyaca",
+    // "gu-IN": "Gujaratça",
+    // "ha-NE": "Hausa",
+    // "hr-HR": "Hırvatça",
+    // "hu-HU": "Macarca",
+    // "id-ID": "Endonezce",
+    // "is-IS": "İzlandaca",
+    // "kk-KZ": "Kazakça",
+    // "cy-GB": "Galce",
+    // "da-DK": "Danca",
+    // "dz-BT": "Dzongkha",
+    // "dv-MV": "Maldivce",
+    // "km-KM": "Kmerce",
+    // "kn-IN": "Kannada",
+    // "ky-KG": "Kırgızca",
+    // "lo-LA": "Laoca",
+    // "lv-LV": "Letonca",
+    // "men-SL": "Mende",
+    // "mg-MG": "Malgaşça",
+    // "mi-NZ": "Maorice",
+    // "ms-MY": "Malayca",
+    // "mt-MT": "Maltaca",
+    // "my-MM": "Burmaca",
+    // "ne-NP": "Nepalce",
+    // "niu-NU": "Niuence",
+    // "nl-NL": "Felemenkçe",
+    // "no-NO": "Norveççe",
+    // "ny-MW": "Nyanja",
+    // "ur-PK": "Urduca",
+    // "pau-PW": "Palauanca",
+    // "pa-IN": "Pencapça",
+    // "ps-PK": "Peştuca",
+    // "pis-SB": "Pijin",
+    // "pl-PL": "Lehçe",
+    // "rn-BI": "Kirundi",
+    // "ro-RO": "Rumence",
+    // "sg-CF": "Sango",
+    // "si-LK": "Seylanca",
+    // "sk-SK": "Slovakça",
+    // "sm-WS": "Samoa Dili",
+    // "sn-ZW": "Shona",
+    // "so-SO": "Somalice",
+    // "sq-AL": "Arnavutça",
+    // "sr-RS": "Sırpça",
+    // "sv-SE": "İsveççe",
+    // "sw-SZ": "Svahili",
+    // "ta-LK": "Tamilce",
+    // "te-IN": "Teluguca",
+    // "tet-TL": "Tetum",
+    // "tg-TJ": "Tacikçe",
+    // "th-TH": "Tayca",
+    // "ti-TI": "Tigrinya",
+    // "tk-TM": "Türkmence",
+    // "tl-PH": "Filipince",
+    // "tn-BW": "Setswana",
+    // "to-TO": "Tongaca",
+    // "uk-UA": "Ukraynaca",
+    // "uz-UZ": "Özbekçe",
+    // "vi-VN": "Vietnamca",
+    // "wo-SN": "Wolofça",
+    // "xh-ZA": "Xhosa",
+    // "yi-YD": "Yidiş",
+    // "zu-ZA": "Zuluca" 
+    // "be-BY": "Belarus",
+    // "bem-ZM": "Bemba",
+    // "bi-VU": "Bislama",
+    // "bjs-BB": "Barbadoslu",
+    // "bn-IN": "Bengalce",
+    // "bo-CN": "Tibetçe",
+    // "br-FR": "Bretonca",
+    // "bs-BA": "Boşnakça",
+    // "ca-ES": "Katalanca",
+    // "cop-EG": "Kıptice",
+
